7a502d45ddeeac22c71902503182c5b3d8a9194a
mdfaisalislam <md.faisal_islam@hotmail.com>
2023-06-25 21:50:19 +0600

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:tutorial_flutter/home_page.dart';
+import 'package:tutorial_flutter/pages/home_page.dart';
+import 'package:tutorial_flutter/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
+  final int days = 30;
+  final String name = "faisal";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,9 +11,10 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-          child: Text("welcome to 30 days of flutter"),
+          child: Text("welcome to $days days of flutter by $name"),
         ),
       ),
+      drawer: Drawer(),
     );
   }
 }
